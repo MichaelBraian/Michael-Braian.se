31df6e2d1ce4ea59438f4245d6ef90f571f619e4
Michael Braian <michaelbraian@Michaels-MBP-2.lan>
2024-10-19 23:48:21 +0200

    Update API routes to use export const runtime = 'edge'

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
